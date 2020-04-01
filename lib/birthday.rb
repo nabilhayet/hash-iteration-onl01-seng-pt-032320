b26d7e9d1200947093e42edc9a35a56c7eb52b93
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-01 03:26:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,3 +3,8 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
 end 
+
+def age_appropriate_birthday(birthday_kids)
+birthday_kids.each do |name,age|
+  
+end
