670b3e5eec1281b0b027edb9e444b2cb6db0042f
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-01 03:31:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@ def happy_birthday(birthday_kids)
 end 
 
 def age_appropriate_birthday(birthday_kids)
-birthday_kids.each do |name,age|
+birthday_kids.collect do |name,age|
   if age <= 12 
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   else
